77e2977aaf57e9bf5267bf3908d4f6bd33959b35
summerLI <1129791659@qq.com>
2017-08-04 18:31:25 +0800

    添加helloWorld

---
+++ src/main/java/onlin/summer/main/main.java
@@ -0,0 +1,10 @@
+package onlin.summer.main;
+
+/**
+ * Created by p2p on 2017/8/4.
+ */
+public class main {
+    public static void main(String [] args){
+        System.out.println("summerThread");
+    }
+}
